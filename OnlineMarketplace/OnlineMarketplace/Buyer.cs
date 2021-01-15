960b2d9d6a94c93d2bd84318df7d7a364e5991cb
HeisenBerk29 <49711170+HeisenBerk29@users.noreply.github.com>
2021-01-15 18:32:59 +0300

    Ability to add balance and display basket for buyers implemented

--- OnlineMarketplace/OnlineMarketplace/Buyer.cs
+++ OnlineMarketplace/OnlineMarketplace/Buyer.cs
@@ -7,14 +7,13 @@ namespace OnlineMarketplace
     public class Buyer : User
     {
         List<Item> basket = new List<Item>();
+        private double totalBasketPrice;
 
         public List<Item> Basket
         {
             get { return basket; }
             set { basket = value; }
         }
-
-                
 
         public Buyer(string UserID, string Location, string Name, string Password):base()
         {
@@ -25,6 +24,36 @@ namespace OnlineMarketplace
             balance = 0;
             userType = "Buyer";
         }
-        
+
+        public double TotalBasketPrice
+        {
+            get { return totalBasketPrice; }
+            set { totalBasketPrice = value; }
+        }
+
+        public void addBalance()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Current Balance: " + balance);
+            Console.WriteLine("Enter the amount to deposit");
+            balance += Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("New Balance: " + balance);
+            Console.WriteLine();
+        }
+
+        public void displayBasket()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Items in the basket: ");
+            for (int i = 0; i < basket.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ") " + basket[i].Name + " " + basket[i].Price + "TL");
+                totalBasketPrice += basket[i].Price;
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("Total Price: " + totalBasketPrice + "TL");
+        }
+
     }
 }
